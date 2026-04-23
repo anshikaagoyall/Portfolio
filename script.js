e656cd77759b24baa0ecf8fe000ccb91c85b0c88
anshikaagoyall <anshikagoyal936@gmail.com>
2026-04-23 13:19:22 +0530

    Initial Portfolio Website

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
